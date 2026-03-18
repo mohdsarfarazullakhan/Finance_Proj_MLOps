abedae7886cecc633cb40a7856830a9b94471625
MD Sarfaraz Khan <mohdsarfarazullakhan@gmail.com>
2026-03-18 17:42:48 +0530

    Project Structure Ready with venv

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sarfaraz khan",
+    author_email="mohdsarfarazullakhan07@gmail.com",
+    packages=find_packages()
+)
